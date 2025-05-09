0eec8eb4c6685478c8d6aabc4d4b1a0c165c0bd2
adzaky15 <adzakymaulana15@gmail.com>
2025-05-09 21:23:36 +0700

    try to add to cart

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,8 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class SampleTest {
 
@@ -11,12 +16,16 @@ public class SampleTest {
     String password = "Test5Email25";
     WebDriver driver;
     String baseUrl = "https://www.periplus.com/ ";
+    String newReleasesUrl = "https://www.periplus.com/index.php?route=product/category&anl=103";
 
     @BeforeMethod
     public void setUpBrowser() {
 
         // Initialize ChromeDriver.
         driver = new ChromeDriver();
+
+        // Set implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         // Maximize the browser window size.
         driver.manage().window().maximize();
@@ -47,6 +56,22 @@ public class SampleTest {
         emailField.sendKeys(email);
         passwordField.sendKeys(password);
         submitLogin.click();
+
+        // Ensure that the home link is present.
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/header/div[3]/div/div/div/div/div/nav/div/div/ul/li[2]/a")));
+        WebElement homeLink = driver.findElement(By.xpath("/html/body/header/div[3]/div/div/div/div/div/nav/div/div/ul/li[2]/a"));
+        Assert.assertTrue(homeLink.isDisplayed(), "The home link was not found.");
+
+        // Navigate to the website new releases page.
+        driver.get(newReleasesUrl);
+
+        // Ensure that the item add to cart is present.
+        WebElement addToCart = driver.findElement(By.xpath("/html/body/section/div/div/div[3]/div[3]/div[1]/div/div[2]/div[5]/div/div/a[2]"));
+        Assert.assertTrue(addToCart.isDisplayed(), "The login link was not found.");
+
+        // Click the item add to cart.
+        addToCart.click();
 
     }
 
