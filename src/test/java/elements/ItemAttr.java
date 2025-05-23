51d5e2b97a6986e31ab5dbf0b683d716ddd2f15e
adzaky15 <adzakymaulana15@gmail.com>
2025-05-23 08:17:29 +0700

    bug fix: remove trailing dot from long titles

--- src/test/java/elements/ItemAttr.java
+++ src/test/java/elements/ItemAttr.java
@@ -7,7 +7,7 @@ public class ItemAttr {
     public int quantity;
 
     public ItemAttr(String title, String url, String price, int quantity) {
-        this.title = (title != null ? title : "").trim();
+        this.title = (title != null ? title : "").trim().replaceAll("\\.+$", "");
         this.url = (url != null ? url : "").trim();
         this.price = (price != null ? price : "").split("or")[0].trim();
         this.quantity = quantity;
